39580322b1a8913cf07a679dfa07861c8501b6c6
Tetsuya Hoshino <hoshino.tetsuya@gmail.com>
2013-07-07 09:39:42 +0900

    css test

--- Symfony/src/nandakke/areBundle/Controller/AreController.php
+++ Symfony/src/nandakke/areBundle/Controller/AreController.php
@@ -1,21 +1,17 @@
 <?php
 
 namespace nandakke\areBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class AreController extends Controller
-{
-    public function indexAction($name)
-    {
-    	$ares = $this->getDoctrine()
-    	->getRepository('areBundle:Are')
-    	->findAll();
+class AreController extends Controller {
+    public function indexAction($name) {
+        $ares = $this->getDoctrine()->getRepository('areBundle:Are')->findAll();
 
-    	if (!$ares) {
-    		throw $this->createNotFoundException('No data found');
-    	}
-
-        return $this->render('areBundle:Are:index.html.twig', array('ares' => $ares));
+        if (!$ares) {
+            throw $this->createNotFoundException('No data found');
+        }
+        return $this
+                ->render('areBundle:Are:index.html.twig',
+                        array('ares' => $ares));
     }
 }
